46e15148afb9068cb082f0d5b8d22813fb083805
naywvi <nagib.lakhdari.pro@gmail.com>
2025-03-25 01:09:47 +0100

    HEAP_SIZE BumpAllocator next constructor new()

--- allocateur_memoire/src/main.rs
+++ allocateur_memoire/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+const HEAP_SIZE: usize = 64 * 1024;
+
+pub struct BumpAllocator {
+    next: AtomicUsize,
+}
+
+impl BumpAllocator {
+    pub const fn new() -> Self {
+        BumpAllocator {
+            next: AtomicUsize::new(0),
+        }
+    }
 }
