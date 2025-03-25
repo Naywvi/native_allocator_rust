c6cc32ae46f0781a08a94c26df107eb84c91b1a4
naywvi <nagib.lakhdari.pro@gmail.com>
2025-03-25 01:11:52 +0100

    AlignedHeap static mut HEAP

--- allocateur_memoire/src/main.rs
+++ allocateur_memoire/src/main.rs
@@ -1,3 +1,8 @@
+#[repr(align(8))]
+struct AlignedHeap([u8; HEAP_SIZE]);
+
+static mut HEAP: AlignedHeap = AlignedHeap([0; HEAP_SIZE]);
+
 use core::sync::atomic::{AtomicUsize, Ordering};
 
 const HEAP_SIZE: usize = 64 * 1024;
